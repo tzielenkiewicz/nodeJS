3ca0354868b1f737aad321387029eb2c2cc6d3f7
tzielenkiewicz <tomasz.jacek.zielenkiewicz@gmail.com>
2022-07-30 15:34:17 +0200

    zmiana tekstu

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Tomek jest super SUPER!');
+  res.end('Tomek jest super ekstra SUPER!');
 }
 
 const server = http.createServer(requestListener);
